c315d125e97542d1087dcf82d6b4dbb2ef9e3cfd
lsaisidd <lsaisidd@unm.edu>
2012-08-29 21:52:29 -0600

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mytest::Application.config.secret_token = '8a1c5a16b865e5f839177d36b8ccfd853ab442b81e6683bfa563762b966b334f037c0376f4e6895db284757a570c5a250f38545e2095b5efecb1a37f08c46147'
+Mytest::Application.config.secret_token = '661c3d644f3cc99c4675cb20f3c3aa297a75fc9150dc1de07c4f9f539bb0e87278b68b34d86cc6081e4c299e7adfe16cdaccc74aacb2a612e123eda96f556ec5'
